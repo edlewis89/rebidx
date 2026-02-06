bb29231872bc32723275bdf526edb44d69557ca7
lewiedw <lewiedw@gmail.com>
2026-02-05 23:18:51 -0500

    Rebidx: Fix Stripe webhook controller Zeitwerk naming

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,21 +1,34 @@
-def stripe
-  payload = request.body.read
-  sig_header = request.env['HTTP_STRIPE_SIGNATURE']
-  event = nil
+class WebhooksController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
-  begin
-    event = Stripe::Webhook.construct_event(
-      payload, sig_header, ENV['STRIPE_WEBHOOK_SECRET']
-    )
-  rescue JSON::ParserError, Stripe::SignatureVerificationError => e
-    return head :bad_request
+  def stripe
+    payload = request.body.read
+    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+    event = nil
+
+    begin
+      event = Stripe::Webhook.construct_event(
+        payload,
+        sig_header,
+        ENV['STRIPE_WEBHOOK_SECRET']
+      )
+    rescue JSON::ParserError, Stripe::SignatureVerificationError
+      return head :bad_request
+    end
+
+    case event.type
+    when 'checkout.session.completed'
+      session = event.data.object
+      handle_successful_payment(session)
+    end
+
+    head :ok
   end
 
-  case event.type
-  when 'checkout.session.completed'
-    session = event.data.object
-    handle_successful_payment(session)
-  end
+  private
 
-  head :ok
+  def handle_successful_payment(session)
+    # TODO: implement payment success logic
+    Rails.logger.info "Stripe payment completed: #{session.id}"
+  end
 end

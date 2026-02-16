095f533d25ee7964cbae17c336903731c1a735a8
lewiedw <lewiedw@gmail.com>
2026-02-15 21:40:34 -0500

    SendGrid fix - add logging

--- app/services/sendgrid_mailer.rb
+++ app/services/sendgrid_mailer.rb
@@ -3,18 +3,20 @@ include SendGrid
 
 class SendgridMailer
   def self.send_email(to:, subject:, html:)
+    Rails.logger.info "SendGrid::SendEmail >>>> "
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-
+    Rails.logger.info "SendGrid::SendEmail >>>> #{ENV['SENDGRID_API_KEY']}"
     from = Email.new(email: 'ed@sixhattechnologies.com')
     to_email = Email.new(email: to)
     content = Content.new(type: 'text/html', value: html)
     mail = Mail.new(from, subject, to_email, content)
+    Rails.logger.info "SendGrid::SendEmail >>>> #{from}"
 
-    Rails.logger.info "Sending confirmation to #{to} via SendGrid"
+    Rails.logger.info "SendGrid::SendEmail >>>> Sending confirmation to #{to} via SendGrid"
     response = sg.client.mail._('send').post(request_body: mail.to_json)
-    Rails.logger.info "SendGrid response: #{response.status_code} #{response.body}"
+    Rails.logger.info "SendGrid::SendEmail >>>> SendGrid response: #{response.status_code} #{response.body}"
 
-    Rails.logger.info "SendGrid response: #{response.status_code}"
+    Rails.logger.info "SendGrid::SendEmail >>>> SendGrid response: #{response.status_code}"
     response
   end
 end
